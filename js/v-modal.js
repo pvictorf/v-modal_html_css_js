4dd50019b5082039d0d48f57862848509047e994
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-07-24 09:10:37 -0300

    Fix bug modal-container and some changes in css

--- js/v-modal.js
+++ js/v-modal.js
@@ -1,25 +1,25 @@
 function openModal(element, preventClose) {
 
-   $(element).removeAttr('class').addClass('four');
+   $(element).removeAttr('class').addClass('modal-container').addClass('four');
    $('body').addClass('modal-active');
 
-   //Evento para fechar o modal
    $(element + ' .modal-close').on('click', function() {
       closeModal(element);
    });
 
-   //Previne fechar modal quando clicar background
    if(!preventClose) {
       $(element + ' .modal-close-area').on('click', function() {
          closeModal(element);
       })
+   } else {
+      $(element + ' .modal-close-area').off('click');
    }
 }
 
 function closeModal(element) {
    $(element).addClass('out');
    setTimeout(function() {
-      $(element).removeClass('four');
+      $(element).removeClass('four').removeClass('out');
       $('body').removeClass('modal-active');
    }, 300) 
 }
